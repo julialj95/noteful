4ba17771d243283c7d9be35d053aaade640c39dc
Julia Johnson <julialj95@gmail.com>
2021-03-17 19:07:54 -0500

    prepared from deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_FOLDERS_ENDPOINT: "http://localhost:8000/api/folders",
-  API_NOTES_ENDPOINT: "http://localhost:8000/api/notes",
+  API_FOLDERS_ENDPOINT: "https://cryptic-forest-87843.herokuapp.com/folders",
+  API_NOTES_ENDPOINT: "https://cryptic-forest-87843.herokuapp.com/notes",
 };
 
 export default config;
