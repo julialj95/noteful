bd026b13cbf17efee28b3b905a1aef89cc4baaab
Julia Johnson <julialj95@gmail.com>
2021-03-19 18:24:05 -0500

    removed trailing slash in endpoints

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   API_FOLDERS_ENDPOINT:
-    "https://cryptic-forest-87843.herokuapp.com/api/folders/",
-  API_NOTES_ENDPOINT: "https://cryptic-forest-87843.herokuapp.com/api/notes/",
+    "https://cryptic-forest-87843.herokuapp.com/api/folders",
+  API_NOTES_ENDPOINT: "https://cryptic-forest-87843.herokuapp.com/api/notes",
   API_KEY: process.env.REACT_APP_API_KEY,
 };
 
