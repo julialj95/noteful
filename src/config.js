73ef8c39a1fbcaaaa3dfbc256285a078552bdb3d
Julia Johnson <julialj95@gmail.com>
2021-03-19 18:10:19 -0500

    added API key

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
-  API_FOLDERS_ENDPOINT: "https://cryptic-forest-87843.herokuapp.com/folders",
-  API_NOTES_ENDPOINT: "https://cryptic-forest-87843.herokuapp.com/notes",
+  API_FOLDERS_ENDPOINT: "https://cryptic-forest-87843.herokuapp.com/folders/",
+  API_NOTES_ENDPOINT: "https://cryptic-forest-87843.herokuapp.com/notes/",
+  API_KEY: process.env.API_KEY,
 };
 
 export default config;
