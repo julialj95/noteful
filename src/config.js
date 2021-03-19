73c3471539f6d028e3af05bb37694add7f0e935c
Julia Johnson <julialj95@gmail.com>
2021-03-19 18:11:20 -0500

    added REACT_APP to env

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   API_FOLDERS_ENDPOINT: "https://cryptic-forest-87843.herokuapp.com/folders/",
   API_NOTES_ENDPOINT: "https://cryptic-forest-87843.herokuapp.com/notes/",
-  API_KEY: process.env.API_KEY,
+  API_KEY: process.env.REACT_APP_API_KEY,
 };
 
 export default config;
